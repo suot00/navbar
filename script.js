08041cee6958ecaa97bdddb3a92970e3531562f4
suot00 <truongkhacsuot661@gmail.com>
2022-07-18 21:17:57 +0700

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const itemMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", function () {
+    itemMenu.style.display = "block"
+});
